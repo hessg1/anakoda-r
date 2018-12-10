7effc7a8b3d2025ea79bd8ae09e8a5831d9ab802
schwf3 <schwf3@bfh.ch>
2018-12-10 10:00:44 +0100

    Tabs

---
+++ shiny/introduction.md
@@ -0,0 +1,4 @@
+# migrEn
+# A project to analyse data from migraine patients.
+Ich bin ein **normaler** Text.
+> Zitat
--- shiny/ui.R
+++ shiny/ui.R
@@ -8,37 +8,65 @@
 #
 
 library(shiny)
+library(markdown)
 
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("migrEn data view"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      tags$h3(textOutput("patname")),
-      numericInput(inputId = "patient", label = "display curve for patient ID", min=0, max=5, step=1, value=4),
+#shinyUI(fluidPage(
 
-      sliderInput(inputId = "amplitude", label = "amplitude", min=0, max=1, value=0.6),
-      sliderInput(inputId = "period", label = "period", min=1, max=50, value=31),
-      sliderInput(inputId = "offset", label = "offset", min=1, max=50, value=12),
-      sliderInput(inputId = "threshold", label = "threshold", min=0, max=0.5, value=0.2),
-      checkboxInput(inputId = "line", label = "display threshold line", value = TRUE),
-  
-      tags$br(),
-      checkboxInput(inputId = "values", label = "show intensity values", value = FALSE),
-      tags$h3("date range:"),
-      checkboxInput(inputId = "autodate", label = "auto", value = TRUE),
-      dateRangeInput(inputId = "daterange", label = NULL, language = "en", separator = " to ")
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("intensity", height=700)
-     # verbatimTextOutput("stats")
-    )
-  )
-))
+# Application title
+# titlePanel("migrEn data view"),
+
+#Navbar begin
+navbarPage("migrEn Data Viewer",
+           #First Tab -> Introduction
+           tabPanel("Summary",
+                    fluidRow(
+                      column(6,
+                             #includeMarkdown("introduction.md")
+                             helpText("Hallo")
+                      ),
+                      column(3,
+                             img(class="img-polaroid",
+                                 src=paste0("/img/bfh_logo_de.png")),
+                             tags$small(
+                               "Source: Photographed at the Bay State Antique ",
+                               "Automobile Club's July 10, 2005 show at the ",
+                               "Endicott Estate in Dedham, MA by ",
+                               a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
+                                 "User:Sfoskett")
+                             )
+                      )
+                    )
+           ),
+           #Second Tab -> Headache (Define UI for application that draws a histogram)
+           tabPanel("Headache",
+                    
+                    # Sidebar with a slider input for number of bins 
+                    sidebarLayout(
+                      sidebarPanel(
+                        tags$h3(textOutput("patname")),
+                        numericInput(inputId = "patient", label = "display curve for patient ID", min=0, max=5, step=1, value=4),
+                        
+                        sliderInput(inputId = "amplitude", label = "amplitude", min=0, max=1, value=0.6),
+                        sliderInput(inputId = "period", label = "period", min=1, max=50, value=31),
+                        sliderInput(inputId = "offset", label = "offset", min=1, max=50, value=12),
+                        sliderInput(inputId = "threshold", label = "threshold", min=0, max=0.5, value=0.2),
+                        checkboxInput(inputId = "line", label = "display threshold line", value = TRUE),
+                        
+                        tags$br(),
+                        checkboxInput(inputId = "values", label = "show intensity values", value = FALSE),
+                        tags$h3("date range:"),
+                        checkboxInput(inputId = "autodate", label = "auto", value = TRUE),
+                        dateRangeInput(inputId = "daterange", label = NULL, language = "en", separator = " to ")
+                      ),
+                      
+                      # Show a plot of the generated distribution
+                      mainPanel(
+                        plotOutput("intensity", height=700)
+                        # verbatimTextOutput("stats")
+                      )
+                    )
+           )
+           
+)
