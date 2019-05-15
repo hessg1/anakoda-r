662f5127f5a145e4a0c8ed679383c531385ab605
Gabriel Hess <hessg1@bfh.ch>
2019-05-15 21:26:49 +0200

    Separating "Tester" from "Teilnehmer" group

--- midataOnFHIR.R
+++ midataOnFHIR.R
@@ -5,22 +5,32 @@
 source('./shiny/midata-helper.R')
 
 # Setting up the fhirClient
-client <- setupMidata("http://test.midata.coop", TRUE)
+client <- setupMidata("http://ch.midata.coop", TRUE)
 
 # make queries
-res <- client$search("Observation", "date=ge2019-04-01")
+res <- client$search("Observation", "date=ge2019-04-30")
 med <- client$search("MedicationStatement", "status=active")
-
-
 
 # extract data
 observations <- extractObservation(res)
 medications <- extractMedication(med)
 
+# separate tester data from participant data
+tester <- c("P-QSGN-AMCP", "P-MQ05-Q3HM", "P-G8OR-RDUB", "P-POBP-PROB", "P-MLHK-K1L5", "P-UUFN-JHBI", "P-REDP-9UUJ", "P-P5VV-4H3R", "P-HGVD-TODT", "P-VHD4-51A9")
+
+tester_obs <- observations[ which(observations$name %in% tester), ]
+tester_med <- medications[which(medications$name %in% tester),]
+observations <- observations[ which(!observations$name %in% tester), ]
+medications <- medications[which(!medications$name %in% tester),]
+observations <- as.data.frame(lapply(observations, function(x) if(is.factor(x)) factor(x) else x)) # ungenutzte Faktor-Ausprägungen entfernen
+medications <- as.data.frame(lapply(medications, function(x) if(is.factor(x)) factor(x) else x)) # ungenutzte Faktor-Ausprägungen entfernen
+rm(tester)
 
 # wie viele Datensätze sind verkehrt?
 summary(observations$wrongDate)
+str(observations$name)
 summary(observations$name)
+summary(medications$name)
 
 # wann wurde gespeichert?
 obsTimestamps <- observations[,c("name", "timestamp")]
@@ -31,5 +41,4 @@ saveTime$hour <- as.numeric(format(saveTime$timestamp, format="%H"))
 
 hist(saveTime$timestamp, breaks = 'days', main="Verlauf der Einträge aller Teilnehmer")
 hist(saveTime$hour,  main="Zu welchen Uhrzeiten wurden die Einträge gespeichert")
-#
-
+hist(summary(observations$name), main="Wie viele Beiträge haben verschiedene User?")
